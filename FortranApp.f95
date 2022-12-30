bec323ee3386be927c368445a31e98fcc4b00f45
Dimitar Spasov <dspasov@zingasoft.com>
2022-12-30 16:27:36 +0200

    feat(calcApp): add checks

--- FortranApp.f95
+++ FortranApp.f95
@@ -1,29 +1,33 @@
 program main
     implicit none
-    Real::a,b
-    Integer::n
+    Real::a,b,d
+    CHARACTER(LEN=1) :: n
     Print*,"Enter a number:"
     Read*,a
     Print*,"Enter other number:"
     Read*,b
-    Print*,"Enter 1 if you want to add"
-    Print*,"Enter 2 if you want to substract"
-    Print*,"Enter 3 if you want to multiplication"
-    Print*,"Enter 4 if you want to divide"
-    Print*,"Enter 5 if you want to power"
+    Print*,"Choose an action: &
+        &1 for +, 2 for -, 3 for *, 4 for /, 5 for pow"
     Read*,n
         Select case(n)
-            case(1)
-                Print*,"Result = ", a+b
-            case(2)
-                Print*,"Result = ", a-b
-            case(3)
-                Print*,"Result = ", a*b
-            case(4)
-                Print*,"Result = ", a/b
-            case(5)
-                Print*,"Result = ", a**b
+            case('1')
+                d=a+b
+            case('2')
+                d=a-b
+            case('3')
+                d=a*b
+            case('4')
+                if (a < b) then
+                    Print*,"you can't divide by zero!"
+                    call EXIT(1)
+                end if
+                d=a/b
+            case('5')
+                d=a**b
             case default
-                Print*,"Invalid input"
+                Print*,"Invalid number!"
+                call EXIT(1)
+                !stop 0
         End Select
+        Write(*,'(a,f8.2)') "Result =", d
 end program main
